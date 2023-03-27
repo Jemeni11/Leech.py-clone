b5ea32cb1fb9281561702ee8ba8b9f8930620369
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-03-27 14:19:47 +0100

    feat(image.py): - Add compress_image() and PIL_Image_to_bytes() functions to optimize image sizes and convert images to bytes, respectively. - Base64 images will be converted to JPEG format in get_image_from_url(). - Leech now saves animated GIFs. This is not perfect yet.

--- ebook/image.py
+++ ebook/image.py
@@ -1,10 +1,9 @@
-# Basically the same as cover.py with some minor differences
 import math
 
+import PIL.Image
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 from base64 import b64decode
-from math import floor
 import textwrap
 import requests
 import logging
@@ -44,6 +43,42 @@ def make_image(
     return output
 
 
+def compress_image(image: BytesIO) -> PIL.Image.Image:
+    image_size = get_size_format(len(image.getvalue()))
+    logger.info(f"Image size: {image_size}")
+
+    big_photo = Image.open(image).convert("RGBA")
+
+    if image_size[-2:] == "MB":
+        target_pixel_count = 2.8114 * 250_000
+        logger.info("Image is MB, compressing with target size of 250KB")
+    elif image_size[-2:] == "KB" and int(float(image_size[:-2])) > 200:
+        target_pixel_count = 2.8114 * 175_000
+        logger.info("Image is greater than 200KB, compressing with target size of 175KB")
+    else:
+        target_pixel_count = 2.8114 * 100_000
+    scale_factor = target_pixel_count / math.prod(big_photo.size)
+    if scale_factor < 1:
+        x, y = tuple(int(scale_factor * dim) for dim in big_photo.size)
+        logger.info(f"Resizing image dimensions from {big_photo.size} to ({x}, {y})")
+        sml_photo = big_photo.resize((x, y), resample=Image.LANCZOS)
+    else:
+        sml_photo = big_photo
+    return sml_photo
+
+
+def PIL_Image_to_bytes(pil_image: PIL.Image.Image, image_format: str, print_new_image_size: bool = False) -> bytes:
+    out_io = BytesIO()
+    if image_format.lower() in ["jpeg", "jpg"]:
+        pil_image.convert("RGB")
+    if image_format.lower().startswith("gif"):
+        pil_image.save(out_io, save_all=True, format=image_format, optimize=True, quality=95, loop=0)
+    pil_image.save(out_io, format=image_format, optimize=True, quality=95)
+    if print_new_image_size:
+        logger.info(f"Final image size: {get_size_format(len(out_io.getvalue()))}")
+    return out_io.getvalue()
+
+
 def get_image_from_url(url: str):
     """
     Basically the same as make_cover_from_url()
@@ -52,34 +87,32 @@ def get_image_from_url(url: str):
         if url.startswith("https://www.filepicker.io/api/"):
             logger.warning("Filepicker.io image detected, converting to Fiction.live image. This might fail.")
             url = f"https://cdn3.fiction.live/fp/{url.split('/')[-1]}?&quality=95"
+        elif url.startswith("data:image") and 'base64' in url:
+            logger.info("Base64 image detected")
+            head, base64data = url.split(',')
+            file_ext = head.split(';')[0].split('/')[1]
+            imgdata = b64decode(base64data)
+            compressed_base64_image = compress_image(BytesIO(imgdata))
+            image_bytes = PIL_Image_to_bytes(compressed_base64_image, file_ext)
+            if file_ext.lower() not in ["jpg", "jpeg", "gif"]:
+                return _convert_to_jpg(image_bytes).read(), "jpeg", "image/jpeg"
+            if file_ext.lower() == "jpg":
+                file_ext = "jpeg"
+            return image_bytes, file_ext, f"image/{file_ext}"
 
         logger.info("Downloading image from " + url)
-
         img = requests.Session().get(url)
         image = BytesIO(img.content)
         image.seek(0)
 
-        image_size = get_size_format(len(image.getvalue()))
-        logger.info(f"Image size: {image_size}")
+        PIL_image = Image.open(image)
+        if PIL_image.format.lower() == "gif":
+            PIL_image = Image.open(image)
+            if PIL_image.info['version'] not in [b"GIF89a", "GIF89a"]:
+                PIL_image.info['version'] = b"GIF89a"
+            return PIL_Image_to_bytes(PIL_image, "GIF"), "gif", "image/gif"
 
-        bigphoto = Image.open(image).convert("RGBA")
-        logger.info(f"Image dimensions: {bigphoto.size}")
-
-        if image_size[-2:] == "MB":
-            target_pixel_count = 2.8114 * 250_000
-            logger.info("Image is MB, compressing with target size of 250KB")
-        elif image_size[-2:] == "KB" and int(float(image_size[:-2])) > 200:
-            target_pixel_count = 2.8114 * 175_000
-            logger.info("Image is greater than 200KB, compressing with target size of 175KB")
-        else:
-            target_pixel_count = 2.8114 * 100_000
-        scale_factor = target_pixel_count / math.prod(bigphoto.size)
-        if scale_factor < 1:
-            x, y = tuple(int(scale_factor * dim) for dim in bigphoto.size)
-            logger.info(f"Resizing image dimensions to ({x}, {y})")
-            sml_photo = bigphoto.resize((x, y), resample=Image.LANCZOS)
-        else:
-            sml_photo = bigphoto
+        sml_photo = compress_image(image)
 
         # Create a new image with a white background
         background_img = Image.new('RGBA', sml_photo.size, "white")
@@ -87,12 +120,7 @@ def get_image_from_url(url: str):
         background_img.paste(sml_photo, (0, 0), sml_photo)
         sml_photo = background_img.convert('RGB')
 
-        out_io = BytesIO()
-        sml_photo.convert("RGB")
-        sml_photo.save(out_io, format="JPEG", optimize=True, quality=95)
-
-        logger.info(f"Final image size: {get_size_format(len(out_io.getvalue()))}")
-        return out_io.getvalue(), "jpeg", "image/jpeg"
+        return PIL_Image_to_bytes(sml_photo, "JPEG", True), "jpeg", "image/jpeg"
 
     except Exception as e:
         logger.info("Encountered an error downloading image: " + str(e))
@@ -104,13 +132,6 @@ def _convert_to_jpg(image_bytestream):
     png_image = BytesIO()
     Image.open(image_bytestream).save(png_image, format="JPEG")
     png_image.name = 'cover.jpeg'
-    png_image.seek(0)
-
-    return png_image
-def _convert_to_png(image_bytestream):
-    png_image = BytesIO()
-    Image.open(image_bytestream).save(png_image, format="PNG")
-    png_image.name = 'image.png'
     png_image.seek(0)
 
     return png_image
@@ -141,21 +162,6 @@ def draw_text_outlined(draw, xy, text, fill=None, font=None, anchor=None):
     draw.text(xy, text=text, fill=fill, font=font, anchor=anchor)
 
 
-def get_image_size(data):
-    img = Image.open(BytesIO(data))
-    owidth, oheight = img.size
-    return owidth, oheight
-
-
-imagetypes = {
-    'jpg': 'image/jpeg',
-    'jpeg': 'image/jpeg',
-    'png': 'image/png',
-    'gif': 'image/gif',
-    'svg': 'image/svg+xml',
-}
-
-
 def get_size_format(b, factor=1024, suffix="B"):
     """
     Scale bytes to its proper byte format
@@ -168,17 +174,6 @@ def get_size_format(b, factor=1024, suffix="B"):
             return f"{b:.2f}{unit}{suffix}"
         b /= factor
     return f"{b:.2f}Y{suffix}"
-
-
-convtype = {'jpg': 'JPEG', 'png': 'PNG'}
-
-
-def normalize_format_name(fmt):
-    if fmt:
-        fmt = fmt.lower()
-        if fmt == 'jpeg':
-            fmt = 'jpg'
-    return fmt
 
 
 if __name__ == '__main__':
